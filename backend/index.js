92f4b75c62ed64aa5bdfeff85b74ef9d97e8ff79
Shubham Goyal <shubhamgoyal7125@gmail.com>
2022-12-22 15:57:16 +0530

    testing the vercel 1

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ connectToMongo();
 var cors = require('cors');
 var app = express();
 app.use(cors());
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
